b5b19a02b424c10b35ee1ffce2203cbce225c2f2
dadovenonye <chinonyenmecha@yahoo.com>
2024-02-20 21:39:23 -0800

    working with API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Python script that uses a REST API to get data, for a given employee ID,
+returns information about his/her TODO list progress.
+"""
+import requests
+import sys
+
+def fetch_todo_list(employee_id):
+    """API url"""
+    url = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
+
+    try:
+        response = requests.get(url)
+
+        if response.status_code = 200:
+            todo_list = response.json()
+            completed_task = [task['task'] for task in todo_list if task['completed']]
+
+            total_task = len(todo_list)
+            num_completed_task = len(completed_task)
+            print(f"Employee {todo_list[0]['userId']} is done with tasks ({num_completed_tasks}/{total_tasks}):")
+            for task_title in completed_tasks:
+                print(f"\t{task_title}")

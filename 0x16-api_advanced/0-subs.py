33b16bd6cccca3fdd13aaea6e3265f56db4ced17
DadoveNonye <98288131+DadoveNonye@users.noreply.github.com>
2024-03-06 05:59:40 +0100

    added comments

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-#  A function that queries the Reddit API 
+"""A function that queries the Reddit API """
 import requests
 def number_of_subscribers(subreddit):
+   """return number of subscribers"""
    url = f"https://www.reddit.com/r/{subreddit}/about.json"
    headers = {"User-Agent": "Mozilla/5.0"}
    response = requests.get(url, headers=headers,  allow_redirects=False)
